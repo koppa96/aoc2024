003d3df6031e0bff3e4d6992b88a5d047b37d093
Koppa Péter <koppa96@windowslive.com>
2024-12-09 23:40:00 +0100

    make solution more elegant

--- src/day9/task1.rs
+++ src/day9/task1.rs
@@ -1,24 +1,43 @@
-use crate::day9::common::{checksum, read_fs};
+use crate::day9::common::read_nums;
 use crate::utils;
+use std::io;
 
 pub fn solve(input_path: String) -> utils::Result {
-  let mut fs = read_fs(input_path)?;
-  compact(&mut fs);
-  println!("{}", checksum(&fs));
+  let mut blocks = read_blocks(input_path)?;
+  compact(&mut blocks);
+  println!("{}", checksum(&blocks));
 
   Ok(())
 }
 
-fn compact(fs: &mut Vec<Option<u32>>) {
+fn read_blocks(input_path: String) -> io::Result<Vec<Option<u32>>> {
+  let nums: Vec<_> = read_nums(input_path)?;
+  let mut blocks: Vec<Option<u32>> = Vec::new();
+  for i in 0..nums.len() {
+    if i % 2 == 0 {
+      for _j in 0..nums[i] {
+        blocks.push(Some(i as u32 / 2));
+      }
+    } else {
+      for _j in 0..nums[i] {
+        blocks.push(None);
+      }
+    }
+  }
+
+  Ok(blocks)
+}
+
+fn compact(blocks: &mut Vec<Option<u32>>) {
   let mut i = 0;
-  let mut j = fs.len() - 1;
+  let mut j = blocks.len() - 1;
 
   while i < j {
-    while i < j && fs[i] != None {
+    while i < j && blocks[i] != None {
       i += 1;
     }
 
-    while i < j && fs[j] == None {
+    while i < j && blocks[j] == None {
       j -= 1;
     }
 
@@ -26,7 +45,18 @@ fn compact(fs: &mut Vec<Option<u32>>) {
       break;
     }
 
-    fs[i] = fs[j];
-    fs[j] = None;
+    blocks[i] = blocks[j];
+    blocks[j] = None;
   }
+}
+
+fn checksum(fs: &Vec<Option<u32>>) -> usize {
+  let mut sum = 0;
+  for i in 0..fs.len() {
+    if let Some(id) = fs[i] {
+      sum += i * id as usize;
+    }
+  }
+
+  sum
 }
